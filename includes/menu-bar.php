02616a6a99238edb889a1c2fd7eea10f492732d7
ClaudioKiko <89706217+ClaudioKiko@users.noreply.github.com>
2022-04-26 14:01:23 -0400

    Ultimo del día
    ultimo

--- includes/menu-bar.php
+++ includes/menu-bar.php
@@ -13,7 +13,7 @@
                 <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
 	<div class="nav-outer">
 		<ul class="nav navbar-nav">
-			<li class="dropdown yamm-fw">
+			<li class="active dropdown yamm-fw">
 				<a href="index.php" data-hover="dropdown" class="dropdown-toggle">Inicio</a>
                
 				
