50feb04947d86c562268964dc736712215651d17
ClaudioKiko <89706217+ClaudioKiko@users.noreply.github.com>
2022-05-02 16:14:13 -0400

    contacto
    contacto

--- contact.php
+++ contact.php
@@ -61,7 +61,7 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
-		<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+	
 		
 		<!-- Favicon -->
 		<link rel="shortcut icon" href="assets/images/favicon.png">
@@ -189,7 +189,8 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 
     <label for="correo">Correo</label>
     <input type="text" id="lname" name="lastname" placeholder="Su correo..">
-
+     
+	
     
     <label for="subject">Mensaje</label>
     <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
