2ff9c5227ff8e973ff37b9d68e3306bf95a01d4f
Luis Ávila <luis.avila095@gmail.com>
2022-05-04 09:58:33 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -61,7 +61,6 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
-		<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 		
 		<!-- Favicon -->
 		<link rel="shortcut icon" href="assets/images/favicon.png">
