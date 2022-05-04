63120b411ec200bf2b932f486d31d7ef8ff1b2f1
ClaudioKiko <89706217+ClaudioKiko@users.noreply.github.com>
2022-05-04 11:38:43 -0400

    Estilo relatores
    estilo

--- relatores.php
+++ relatores.php
@@ -144,6 +144,62 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
   display: table;
 }
 
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+
 </style>
 </head>
  <body class="cnt-home" onload="myFunction()" style="margin:0;">
@@ -180,41 +236,95 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 
     </div>
 
-<div class="container">
+
+    <div class="row">
   <div class="column">
-    <h2>Alejandra Andrea Carrasco</h2>
-    <p>Profesora</p>
-    <img src="img/profe1.png" alt="Avatar" class="image" width="290" height="290">
+    <div class="card">
+      <img src="img/profe1.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Alejandra Carrasco</h2>
+        <p class="title">Profesora</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        
+      </div>
+    </div>
   </div>
+
   <div class="column">
-    <h2>Claudio Andrés Millan</h2>
-    <p>Prevencionista</p>
-    <img src="img/profe2.png" alt="Avatar" class="image" width="290" height="290">
-   </div>
-   <div class="column">
-    <h2>Ana María Daronch</h2>
-    <p>Kinesiologa</p>
-    <img src="img/profe3.png" alt="Avatar" class="image" width="290" height="290">
-   </div>
+    <div class="card">
+      <img src="img/profe2.png" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Claudio Andrés Millan</h2>
+        <p class="title">Prevencionista</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+       
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="img/profe3.png" alt="John" style="width:100%">
+      <div class="container">
+        <h2>Ana María Daronch</h2>
+        <p class="title">Kinesiologa</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+       
+      </div>
+    </div>
+  </div>
 </div>
 
-<div class="container">
+
+<div class="row">
   <div class="column">
-    <h2>Catherine Inostroza</h2>
-    <p>Enfermera</p>
-    <img src="img/profe4.png" alt="Avatar" class="image" width="290" height="290">
+    <div class="card">
+      <img src="img/profe4.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Catherine Inostroza</h2>
+        <p class="title">Enfermera</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        
+      </div>
+    </div>
   </div>
+
   <div class="column">
-    <h2>Ethelyn Pinto</h2>
-    <p>Psicóloga</p>
-    <img src="img/profe5.png" alt="Avatar" class="image" width="290" height="290">
-   </div>
-   <div class="column">
-    <h2>Francisco Wladimir Briones</h2>
-    <p>Psicólogo</p>
-    <img src="img/profe6.png" alt="Avatar" class="image" width="290" height="290">
-   </div>
+    <div class="card">
+      <img src="img/profe5.png" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Ethelyn Pinto</h2>
+        <p class="title">Psicóloga</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+       
+      </div>
+    </div>
+  </div>
+
+   
+  <div class="column">
+    <div class="card">
+      <img src="img/profe6.png" alt="John" style="width:100%">
+      <div class="container">
+        <h2>Francisco Briones</h2>
+        <p class="title">Psicólogo</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+       
+      </div>
+    </div>
+  </div>
 </div>
+ 
+
+
+
+   
 
 <div class="container">
   <div class="column">
